2dd05c65a33ed4f46aa330b6d3152d84879e3d83
unknown <701tkdgns@inu.ac.kr>
2022-06-27 18:04:05 +0900

    0627

---
+++ main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    sliderPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    speed: 1000,
+    pagination: {
+        el:'.swiper-pagination',
+        type: 'progressbar',
+    }
+});
+
